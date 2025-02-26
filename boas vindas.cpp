91f2fcc9cf304177e8611d19cefb78742de70b0d
weverton119 <weverton7055@gmail.com>
2025-02-26 19:30:54 -0300

    Delete boas vindas.cpp

--- boas vindas.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main (){
-	printf("===================================\n");
-	printf("=====BOAS VINDAS A LINGUAGEM C=====\n");
-	printf("===================================\n");
-	system("pause");
-}
